35eb390fd1ac1faecf87ab8f6b966b980b6bfc21
Ryanwolf <cksdud7158@naver.com>
2020-11-30 16:31:02 +0900

    20.11.30

---
+++ frontcode/src/assets/js/fi/fi002.js
@@ -0,0 +1,47 @@
+export default {
+  data() {
+    return {
+      studio: "",
+      VRLink: "https://my.matterport.com/show/?m=cVA8ESKKouc",
+    };
+  },
+  mounted() {
+    this.studio = JSON.parse(sessionStorage.getItem("studio"));
+  },
+  computed: {
+    wHeight() {
+      let wHeight = window.innerHeight;
+      return wHeight + "px";
+    },
+  },
+  methods: {
+    goBack() {
+      this.$router.push("/bo001");
+    },
+    showVR(i) {
+      switch (i) {
+        case 1:
+          this.VRLink = "https://my.matterport.com/show/?m=cVA8ESKKouc";
+          break;
+        case 2:
+          this.VRLink = "https://my.matterport.com/show/?m=riW2natWs9W";
+          break;
+        case 3:
+          this.VRLink = "https://my.matterport.com/show/?m=riW2natWs9W";
+          break;
+        case 4:
+          this.VRLink = "https://my.matterport.com/show/?m=E74WWkd5R7K";
+          break;
+        case 5:
+          this.VRLink = "https://my.matterport.com/show/?m=dqMjU7ywEcb";
+          break;
+        default:
+          this.VRLink = "https://my.matterport.com/show/?m=WTn7TVqeeEm&back=1";
+          break;
+      }
+    },
+    goReserve() {
+      this.$router.push("/pa001");
+    },
+  },
+};
--- frontcode/src/views/test.vue
+++ frontcode/src/views/test.vue
@@ -1,25 +1,72 @@
 <template>
-  <div id="app">
-    <div class="popup"></div>
-  </div>
+  <v-item-group mandatory>
+    <v-sheet class="overflow-y-auto" max-height="600" tile>
+      <v-item
+        v-for="[c, text, size, weight, spacing, margin] in classes"
+        :key="c"
+      >
+        <template v-slot:default="{ active, toggle }">
+          <v-sheet
+            :color="
+              active
+                ? $vuetify.theme.dark
+                  ? 'grey darken-3'
+                  : 'grey lighten-4'
+                : undefined
+            "
+            class="px-4 pt-4 pb-3"
+            tile
+            style="cursor: pointer;"
+            @click="toggle"
+          >
+            <div
+              :class="[`text-${c}`, active && `mb-${margin}`]"
+              class="transition-swing"
+              v-text="text"
+            ></div>
+
+            <v-expand-transition>
+              <v-responsive v-if="active" max-width="350">
+                <div class="d-flex justify-space-between caption">
+                  <div>
+                    <div class="grey--text mb-2">
+                      Font
+                    </div>
+
+                    <div class="font-weight-medium">
+                      Roboto
+                    </div>
+                  </div>
+
+                  <div>
+                    <div class="grey--text mb-2">
+                      Weight
+                    </div>
+
+                    <div class="font-weight-medium" v-text="weight"></div>
+                  </div>
+
+                  <div>
+                    <div class="grey--text mb-2">
+                      Size
+                    </div>
+
+                    <div class="font-weight-medium" v-text="size"></div>
+                  </div>
+
+                  <div>
+                    <div class="grey--text text-darken-1 mb-2">
+                      Letter spacing
+                    </div>
+
+                    <div class="font-weight-medium" v-text="spacing"></div>
+                  </div>
+                </div>
+              </v-responsive>
+            </v-expand-transition>
+          </v-sheet>
+        </template>
+      </v-item>
+    </v-sheet>
+  </v-item-group>
 </template>
-
-<script>
-export default {};
-</script>
-
-<style scoped>
-.popup {
-  width: 500px;
-  height: 500px;
-  background: red;
-  position: absolute;
-  left: 50%;
-  top: 50%;
-
-  /* 방법1 : margin-left:-너비/2 margin-top:-높이/
-  2 *
-  /* margin-left:-250px; margin-top:-250px; */
-  transform: translate(-50%, -50%);
-}
-</style>
