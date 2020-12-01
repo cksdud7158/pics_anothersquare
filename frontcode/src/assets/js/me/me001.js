73b412210d425258399bafe02c1288f67f8d8b62
Ryanwolf <cksdud7158@naver.com>
2020-12-01 12:21:50 +0900

    20.12.01

--- frontcode/src/assets/js/me/me001.js
+++ frontcode/src/assets/js/me/me001.js
@@ -1,88 +1,73 @@
 export default {
-    data: () => ({
-        valid: false,
-        email: "",
-        emailRules: [
-            (v) => !!v || "E-mail is required",
-            (v) => /.+@.+/.test(v) || "E-mail must be valid",
-        ],
-        password: "",
-        confirmPassword: "",
-        validPassword: "",
-        rules: {
-            passwordRules: (v) => !!v || "Password is required",
-            confirmPasswordRules: (v) => !!v || "Password is required",
-        },
-        customer: "",
-        loginCheck: false,
-        user: {
-            user_email: "",
-            user_password: "",
-        },
-    }),
-    mounted() {
-        this.loginCheckMethood();
+  data: () => ({
+    valid: false,
+    email: "",
+    emailRules: [
+      (v) => !!v || "E-mail is required",
+      (v) => /.+@.+/.test(v) || "E-mail must be valid",
+    ],
+    password: "",
+    confirmPassword: "",
+    validPassword: "",
+    rules: {
+      passwordRules: (v) => !!v || "Password is required",
+      confirmPasswordRules: (v) => !!v || "Password is required",
     },
-    computed: {
-        wHeight() {
-            let wHeight = window.innerHeight;
-            return wHeight + "px";
-        },
-        divHeight() {
-            let divHeight = window.innerHeight;
-            return divHeight / 10 + "px";
-        },
+    customer: "",
+    loginCheck: false,
+    user: {
+      user_email: "",
+      user_password: "",
     },
-    methods: {
-        loginEnter(email, password) {
-            this.clickLogin(email, password);
-        },
-        clickLogin(email, password) {
-            this.$axios
-                .post("http://" + this.$store.state.ipAddress + ":7777/login", {
-                    params: {
-                        email: email,
-                        password: password,
-                    },
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                })
-                .then((response) => {
-                    this.customer = response.data;
-                    console.log(response);
-                })
-                .catch((error) => {
-                    console.log(error);
-                    this.errored = true;
-                })
-                .finally(() => {
-                    this.userConfirm();
-                    this.loading = false;
-                });
-        },
-        userConfirm() {
-            if (this.customer.email == "") {
-                this.loginCheck = true;
-            } else if (this.customer.email != this.email) {
-                this.loginCheck = true;
-            } else {
-                if (this.password != this.customer.password) {
-                    this.loginCheck = true;
-                } else {
-                    this.$store.commit("addUser", this.customer);
-                    this.$router.push("/me005");
-                }
-            }
-        },
-        register() {
-            this.$router.push("/me002");
-        },
-        loginCheckMethood() {
-            console.log(this.$store.state.loginCheck);
-            if (this.$store.state.loginCheck == true) {
-                this.$router.push("/me005");
-            }
-        },
+  }),
+  mounted() {
+    this.loginCheckMethood();
+  },
+  computed: {
+    wHeight() {
+      let wHeight = window.innerHeight;
+      return wHeight + "px";
     },
+    divHeight() {
+      let divHeight = window.innerHeight;
+      return divHeight / 10 + "px";
+    },
+  },
+  methods: {
+    loginEnter(email, password) {
+      this.clickLogin(email, password);
+    },
+    async clickLogin(email, password) {
+      var sn = await this.$axios.post(
+        "http://" + this.$store.state.ipAddress + ":7777/login",
+        {
+          params: {
+            email: email,
+          },
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      this.customer = sn.data;
+      this.userConfirm(password);
+    },
+    userConfirm(password) {
+      if (password == this.customer.password) {
+        this.$store.commit("addUser", this.customer);
+        this.$router.push("/me005");
+      } else {
+        this.loginCheck = true;
+      }
+    },
+    register() {
+      this.$router.push("/me002");
+    },
+    loginCheckMethood() {
+      console.log(this.$store.state.loginCheck);
+      if (this.$store.state.loginCheck == true) {
+        this.$router.push("/me005");
+      }
+    },
+  },
 };
